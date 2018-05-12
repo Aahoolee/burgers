8f47a3d3ae97d431886f92b1db7f361d58c2fdc4
aahoolee <anviento@gmail.com>
2018-05-12 15:24:19 +0300

    add map

--- js/main.js
+++ js/main.js
@@ -12,3 +12,75 @@ function openMenu() {
 
 openMenu();
 
+//////
+ymaps.ready(init);
+
+function init() {
+    var map = new ymaps.Map('map',{
+        center: [59.94, 30.32],
+        zoom: 11,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    var placemark = new ymaps.Placemark([59.97, 30.31], {
+        hintContent: '<div class="map__hint">ул. Литераторов, д. 19 </div>',
+        balloonContent: [
+            '<div class="map__balloon">Бургерная на ул. Литераторов, д. 19, ' +
+            'тел. (812) 333-0085 </div>'
+        ]
+    },
+    {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/icons/map-marker.svg',
+        iconImageSize: [46, 57]
+    });
+
+    var placemark1 = new ymaps.Placemark([59.88, 30.31], {
+            hintContent: '<div class="map__hint">Московский пр., д. 109 </div>',
+            balloonContent: [
+                '<div class="map__balloon">Бургерная на Московском, 109, ' +
+                'тел. (812) 333-0086 </div>'
+            ]
+        },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icons/map-marker.svg',
+            iconImageSize: [46, 57]
+        });
+
+    var placemark2 = new ymaps.Placemark([59.94, 30.38], {
+            hintContent: '<div class="map__hint">ул. Литераторов, д. 19 </div>',
+            balloonContent: [
+                '<div class="map__balloon">Бургерная на ул. Литераторов, д. 19, ' +
+                'тел. (812) 333-0085 </div>'
+            ]
+        },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icons/map-marker.svg',
+            iconImageSize: [46, 57]
+        });
+
+    var placemark3 = new ymaps.Placemark([59.941, 30.49], {
+            hintContent: '<div class="map__hint">ул. Литераторов, д. 19 </div>',
+            balloonContent: [
+                '<div class="map__balloon">Бургерная на ул. Литераторов, д. 19, ' +
+                'тел. (812) 333-0085 </div>'
+            ]
+        },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icons/map-marker.svg',
+            iconImageSize: [46, 57]
+        });
+
+
+    map.geoObjects.add(placemark);
+    map.geoObjects.add(placemark1);
+    map.geoObjects.add(placemark2);
+    map.geoObjects.add(placemark3);
+
+
+
+}
